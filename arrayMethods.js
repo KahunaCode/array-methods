d23721b8a67caffae53d45ad556b5e7bb48b6718
James Moore <jameslmoore@gmail.com>
2017-05-13 19:41:08 -1000

    push() done

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,9 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = [];
+nameArray.push("james", "moore");
+for(var i =6; i<=10; i++){
+  genericNumberArray.push(i);
+}
